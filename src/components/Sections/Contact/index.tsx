003fc3f4ecc6fa18ceb76716f34cb9bd0b9d57ab
Welton Nogueira <welton.nogueira.dev@gmail.com>
2022-09-20 00:23:16 -0300

    Email Updated

--- src/components/Sections/Contact/index.tsx
+++ src/components/Sections/Contact/index.tsx
@@ -12,7 +12,7 @@ export const Contact = () => {
       <Heading fontSize={["2xl", "3xl"]}>CONTATO</Heading>
       <Stack mt="8" fontSize={["sm", "md"]}>
           <Flex gap="2">
-            <Text fontWeight="bold">E-mail:</Text><Text color="zinc.300">welton.nogueira201@gmail.com</Text>
+            <Text fontWeight="bold">E-mail:</Text><Text color="zinc.300">welton.nogueira.dev@gmail.com</Text>
           </Flex>
           <Flex gap="2">
             <Text fontWeight="bold">Telefone:</Text><Text color="zinc.300">(21) 97535-8927</Text>
