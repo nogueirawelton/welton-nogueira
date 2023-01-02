2ade640a211276df82dc8c973289d10b3d6a4733
Welton Nogueira <welton.nogueira.dev@gmail.com>
2023-01-02 19:01:06 -0300

    Change Experience Order

--- src/hooks/useExperience.tsx
+++ src/hooks/useExperience.tsx
@@ -1,6 +1,11 @@
-import { createContext, useContext,useState, ReactNode, useEffect } from "react";
-import { prismic } from "../services/prismic";
-
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { prismic } from '../services/prismic';
 
 interface Experience {
   id: string;
@@ -24,52 +29,66 @@ interface ExperienceContextData {
 const experienceContext = createContext({} as ExperienceContextData);
 
 export function ExperiencesProvider({ children }: ExperienceProviderProps) {
-  const [activeCompany, setActiveCompany] = useState("");
+  const [activeCompany, setActiveCompany] = useState('');
   const [experiences, setExperiences] = useState<Experience[]>([]);
 
   useEffect(() => {
-    prismic.getByType("experience").then(({results}) => {
-      const formattedExperiences = results.map(result => {
-        const { id, data: { company, role, start_date, end_date, description } } = result;
-        return {
-          id,
-          company,
-          role,
-          description,
-          start_date: new Date(start_date).toLocaleString("pt-BR", {
-            day:"2-digit",
-            month:"2-digit",
-            year: "2-digit"
-          }),
-          end_date: result.data.end_date ? new Date(end_date).toLocaleString("pt-BR", {
-            day:"2-digit",
-            month:"2-digit",
-            year: "2-digit"
-          }) : "",
-        };
+    prismic
+      .getByType('experience', {
+        orderings: {
+          field: 'document.first_publication_date',
+          direction: 'desc',
+        },
+      })
+      .then(({ results }) => {
+        const formattedExperiences = results.map((result) => {
+          const {
+            id,
+            data: { company, role, start_date, end_date, description },
+          } = result;
+          return {
+            id,
+            company,
+            role,
+            description,
+            start_date: new Date(start_date).toLocaleString('pt-BR', {
+              day: '2-digit',
+              month: '2-digit',
+              year: '2-digit',
+            }),
+            end_date: result.data.end_date
+              ? new Date(end_date).toLocaleString('pt-BR', {
+                  day: '2-digit',
+                  month: '2-digit',
+                  year: '2-digit',
+                })
+              : '',
+          };
+        });
+        setExperiences(formattedExperiences);
+        setActiveCompany(formattedExperiences[0].id);
       });
-      setExperiences(formattedExperiences);
-      setActiveCompany(formattedExperiences[0].id);
-    });
-  }, [])
+  }, []);
 
-  const activeExperienceData = experiences.find(xp => xp.id === activeCompany);
-
+  const activeExperienceData = experiences.find(
+    (xp) => xp.id === activeCompany
+  );
 
   function setCompany(id: string) {
     setActiveCompany(id);
   }
 
   return (
-    <experienceContext.Provider value={{
-      experiences,
-      activeCompany,
-      activeExperienceData,
-      setCompany
-    }}>
+    <experienceContext.Provider
+      value={{
+        experiences,
+        activeCompany,
+        activeExperienceData,
+        setCompany,
+      }}>
       {children}
     </experienceContext.Provider>
-  )
+  );
 }
 
 export function useExperience() {
